ba0b022e210b899007003dc379511a08f266b115
professorkoala <59692879+professorkoala@users.noreply.github.com>
2020-01-09 22:09:11 +0100

    guess-the-number-v2.py

--- guess-the-secret-number.py
+++ guess-the-secret-number.py
@@ -1,6 +1,14 @@
-secret = 62
-guess = int(input("Guess the secret number: "))
-if guess == secret:
-    print("Congratulations, you guessed the number!")
-else:
-    print("That is not the correct number.")
+import random
+
+secret = random.randint(1, 30)
+
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+
+    if guess == secret:
+        print("Congratulations, you guessed the number! It was " + str(secret))
+        break
+    elif guess < secret:
+        print(str(guess) + " is smaller than the secret number.")
+    elif guess > secret:
+        print(str(guess) + " is bigger than the secret number.")
