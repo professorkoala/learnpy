0e2f51f5ce6dbc337f72db92336eea078c6702fc
professorkoala <59692879+professorkoala@users.noreply.github.com>
2020-01-09 17:30:32 +0100

    Add files via upload

---
+++ guess the secret number.py
@@ -0,0 +1,6 @@
+secret = 62
+guess = int(input("Guess the secret number: "))
+if guess == secret:
+    print("Congratulations, you guessed the number!")
+else:
+    print("That is not the correct number.")
