99157da6a794a4f32c36e3d992c7f34204e74072
professorkoala <59692879+professorkoala@users.noreply.github.com>
2020-01-09 17:38:32 +0100

    Create mood-checker.py

---
+++ mood-checker.py
@@ -0,0 +1,14 @@
+mood = input("What mood are you in: ")
+
+if mood == "happy":
+    print("It's great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up, mate!")
+elif mood == "excited":
+    print("You go girl!")
+elif mood == "relaxed":
+    print("Enjoy the peace.")
+else:
+    print("I don't recognize this mood.")
