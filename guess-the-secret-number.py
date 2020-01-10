c8356b11ce29cb9f79791543c19426b89b0b3448
professorkoala <59692879+professorkoala@users.noreply.github.com>
2020-01-10 14:15:33 +0100

    guess-the-secret-number-v3

--- guess-the-secret-number.py
+++ guess-the-secret-number.py
@@ -1,12 +1,22 @@
 import random
 
 secret = random.randint(1, 30)
+attempts = 0
+
+with open("score.txt", "r") as score_file:
+    best_score = int(score_file.read())
+    print("Top score: " + str(best_score))
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
 
     if guess == secret:
         print("Congratulations, you guessed the number! It was " + str(secret))
+        print("Your attempts: " + str(attempts))
+        if attempts < best_score:
+            with open ("score.txt", "w") as score_file:
+                score_file.write(str(attempts))
         break
     elif guess < secret:
         print(str(guess) + " is smaller than the secret number.")
